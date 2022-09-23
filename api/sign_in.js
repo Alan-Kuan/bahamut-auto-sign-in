2de0ec14d4ba4e90a61d769df79bb8009ec045d2
Alan-Kuan <kuanhaoan@gmail.com>
2022-09-23 12:55:51 +0800

    style: make month 2-digit

--- api/sign_in.js
+++ api/sign_in.js
@@ -15,7 +15,11 @@ login(req, env.UID, env.PASSWD, env.VCODE)
         const signin_msg = await sign_in(req);
         const guild_signin_msg = await guild_sign_in(req);
         const ani_answer_msg = await ani_answer(req);
-        const today = new Date().toLocaleDateString('zh-TW').replaceAll('/', '-');
+        const today = new Date().toLocaleDateString('zh-TW', {
+            year: 'numeric',
+            month: '2-digit',
+            day: '2-digit',
+        }).replaceAll('/', '-');
         const msg = `[登入簽到]\n${signin_msg}\n\n` +
             `[公會簽到]\n${guild_signin_msg}\n\n` +
             `[動畫瘋問答遊戲]\n${ani_answer_msg}\n\n` +
